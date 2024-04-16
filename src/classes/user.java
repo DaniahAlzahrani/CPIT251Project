7570acf083aa8fcc5daca60e702cb43d29835d6d
Raneem <ri7v1423@hotmail.com>
2024-04-16 11:47:28 +0300

    user class

---
+++ src/classes/user.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author raneemAljehani
+ */
+public class user {
+    
+    private String name;
+    private String phoneNumber;
+    private String password;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
