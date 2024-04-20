77a836e24fe11d4c6ef1208c5a6b3dc521452d0c
Dania <danihadel@hotmail.com>
2024-04-20 17:27:14 +0300

    Sessionm class

--- src/classes/Session.java
+++ src/classes/Session.java
@@ -13,6 +13,15 @@ public class Session {
     private int sessionNumber;
     private String sessionType;
     private String date;
+    private boolean status;
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 
     public void displaySessions() {
         System.out.println("Displaying sessions.");
