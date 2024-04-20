fa9ac58bb2acdac054fa6c80377cc5b74a422ce1
Raneem <ri7v1423@hotmail.com>
2024-04-20 16:57:04 +0300

    user class

---
+++ src/classes/Trainer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author raneemAljehani
+ */
+public class Trainer extends user{
+      private String password;
+      
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
