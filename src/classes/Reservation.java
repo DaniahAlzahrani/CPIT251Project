b0d2bd687200279d9af893abe6ceb242a0381a31
Hamdah <tony-altawi@hotmail.com>
2024-04-20 18:24:43 +0300

    resrvation class

---
+++ src/classes/Reservation.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hamdah-
+ */
+class Reservation {
+     public static ArrayList<Reservation> reserve = new ArrayList<>() ;
+     
+
+//array list hisory 
+   
+    public Reservation( Session session ) {
+    }
+    
+}
